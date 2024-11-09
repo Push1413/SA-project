f91b2fb88be4706caa8b56b35d30684793140675
Pushkar Aagnihotri <agnihotripush@gmail.com>
2024-11-09 11:37:09 -0800

    Update loaders.js

--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -1,10 +1,10 @@
 import { defer } from "react-router-dom";
 import apiRequest from "./apiRequest";
 
-// export const singlePageLoader = async ({ request, params }) => {
-//   const res = await apiRequest("/post/" + params.id);
-//   return res.data;
-// };
+export const singlePageLoader = async ({ request, params }) => {
+  const res = await apiRequest("/post/" + params.id);
+  return res.data;
+};
 export const listPageLoader = async ({ request, params }) => {
   const query = request.url.split("?")[1];
   const postPromise = apiRequest("/post?" + query);
@@ -13,11 +13,11 @@ export const listPageLoader = async ({ request, params }) => {
   });
 };
 
-// export const profilePageLoader = async () => {
-//   const postPromise = apiRequest("/users/profilePosts");
-//   const chatPromise = apiRequest("/chats");
-//   return defer({
-//     postResponse: postPromise,
-//     chatResponse: chatPromise,
-//   });
-// };
+export const profilePageLoader = async () => {
+  const postPromise = apiRequest("/users/profilePosts");
+  const chatPromise = apiRequest("/chats");
+  return defer({
+    postResponse: postPromise,
+    chatResponse: chatPromise,
+  });
+};
