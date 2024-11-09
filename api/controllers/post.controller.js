326096d7d2e290a121b53f06016982be13b27b5f
Pushkar Aagnihotri <agnihotripush@gmail.com>
2024-11-09 02:22:48 -0800

    cors fix

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,51 @@
+import prisma from "../lib/prisma.js";
+
+export const getPosts = async (req, res) => {
+  console.log("router works");
+    // const query = req.query;
+
+    // try {
+    //   const posts = await prisma.post.findMany({
+    //     where: {
+    //       city: query.city || undefined,
+    //       type: query.type || undefined,
+    //       property: query.property || undefined,
+    //       bedroom: parseInt(query.bedroom) || undefined,
+    //       price: {
+    //         gte: parseInt(query.minPrice) || undefined,
+    //         lte: parseInt(query.maxPrice) || undefined,
+    //       },
+    //     },
+    //   });
+  
+    //   // setTimeout(() => {
+    //   res.status(200).json(posts);
+    //   // }, 3000);
+    // } catch (err) {
+    //   console.log(err);
+    //   res.status(500).json({ message: "Failed to get posts121" });
+    // }
+};
+
+export const getPost = async (req, res) => {
+    const id = req.params.id;
+    try {
+      const post = await prisma.post.findUnique({
+        where: { id },
+        include: {
+          postDetail: true,
+          user: {
+            select: {
+              username: true,
+              avatar: true,
+            },
+          },
+        },
+      });
+   
+      res.status(200).json({ ...post, isSaved: false });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Failed to get post" });
+    }
+};
