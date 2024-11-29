e96fe48b003d6913da40fc8f6c6c755d83d41ac4
Pushkar Aagnihotri <agnihotripush@gmail.com>
2024-11-28 16:33:33 -0800

    client env

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import {
 } from "react-router-dom";
 import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
-import SinglePage from "./routes/singlePage/singlePage";
+import SinglePage from "./routes/singlePage/SinglePage";
 import ProfilePage from "./routes/profilePage/profilePage";
 import Login from "./routes/login/login";
 import Register from "./routes/register/register";
